ea63dde51ceaf77ce36bc387046b34fb58b8acd6
Mohammad Momeni <59508036+mohammadmomeni@users.noreply.github.com>
2021-07-16 12:03:46 +0430

    Crawl all page links ThreadPool or normal for loop for "children" prop

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 from enum import unique
-import time, json , datetime
+import time, json, datetime
 from utils import is_url_root, url_spliter, list_counter
 import crawl_utils
 from text_process_utils import TextSimilarity
+from concurrent.futures import ThreadPoolExecutor
 
 URL = "https://www.zoomg.ir/"
 URL_PAGE = "https://www.zoomit.ir/howto/372640-web-searches-secure-private/"
@@ -13,22 +14,27 @@ class WebPage:
     def __init__(
         self,
         url: str,
+        # crawl_pages : bool = False
     ) -> None:
         is_first_site_page = False
+
         if is_url_root(url):
             is_first_site_page = True
 
         t1 = time.time()
+        print(f"Crawling {url}")
 
         response = crawl_utils.safe_get(url)
         soup = crawl_utils.page_soup(response)
         links = crawl_utils.all_links_of(soup, root_url=url)
         title = soup.title.string
+        title_h1 = soup.find("h1").text.strip() if soup.find("h1") else None
 
         crawl_time_in_seconds = round(time.time() - t1, 3)
 
         self.title = title
         self.ready_title = title.split("-")[0].strip()
+        self.title_h1 = title_h1
         self.url = url
         self.links = links
         self.just_links = [link["href"] for link in self.links]
@@ -36,6 +42,8 @@ class WebPage:
         self.is_first_site_page = is_first_site_page
         self.soup = soup
         self.crawled_date = datetime.datetime.now()
+        self._children = []
+        self.children_crawl_time_seconds = None
 
     def most_repeated_paths(self, length: int = 5):
         second_url_children = [
@@ -57,16 +65,18 @@ class WebPage:
 
     def article_content(self):
         el = self.get_article_element()
-        text = el.text.strip()
+        text = el.text.strip() if el else None
         return text
 
     def get_article_image_element(self):
         all_images = self.get_all_images()
+        ts = TextSimilarity()
         for img in all_images:
-            if (
-                img.get("alt", "").strip() != ""
-                and TextSimilarity().is_similar_to(img["alt"], self.title)[0]
-            ):
+            img_alt = img.get("alt", "").strip()
+            _title = self.title_h1 or self.ready_title
+            is_similar, similarity = ts.is_similar_to(img_alt, _title)
+            self.img_alt_similarity_with_title = similarity * 100
+            if img_alt != "" and is_similar:
                 return img
         return None
 
@@ -81,17 +91,47 @@ class WebPage:
         article_content_length_is_enough = len(page_article_element.text) >= 350
         return article_content_length_is_enough
 
+    def crawl_children(self, multithread: bool = True, limit: int = None):
+        t1 = time.time()
+        _links = self.just_links if not limit else self.just_links[0:limit]
+        if not multithread:
+            self._children = [WebPage(link) for link in _links]
+        else:
+
+            def maper(url: str):
+                page = WebPage(url)
+                self._children.append(page)
+
+            with ThreadPoolExecutor() as executor:
+                executor.map(maper, _links)
+        self.children_crawl_time_seconds = round(time.time() - t1, 3)
+
+    def children(self, multithread: bool = True, limit: int = None) -> list:
+        self.crawl_children(multithread=multithread, limit=limit)
+        return self._children
+
     def json(self):
+        main_img = self.get_article_image_element()
+        main_img_src = main_img["src"] if main_img else None
         _dict = {
-            "title": self.ready_title,
             "url": self.url,
-            "img": [self.get_article_image_element()["src"]],
-            "is_first_page": self.is_first_site_page,
+            "title": self.ready_title,
+            "title_h1": self.title_h1,
+            "links_count": len(self.just_links),
+            "img": [main_img_src] if main_img_src else None,
+            "is_root_page": self.is_first_site_page,
+            "is_article_page": self.is_article_page(),
             "crawl_time_seconds": self.crawl_time_seconds,
-            "article_content": self.article_content(),
+            "crawled_date": self.crawled_date,
+            "img_alt_similarity_with_title": self.img_alt_similarity_with_title
+            # "article_content": self.article_content(),
         }
-        return json.dumps(_dict)
+        return json.dumps(_dict, indent=4, default=str)
 
 
 p = WebPage(url=URL)
-print(p.crawled_date)
+for page in p.children(multithread=True, limit=5):
+    print(page.json())
+
+print("-------\n")
+print(p.children_crawl_time_seconds)
