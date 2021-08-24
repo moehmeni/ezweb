570fb89bc868407a2cadcdc7e3f8d0fcfcc17580
Mohammad Momeni <moehmeni@gmail.com>
2021-08-24 11:59:22 +0430

    Remove unused utils

--- utils/text.py
+++ utils/text.py
@@ -1,4 +1,3 @@
-from statistics import mean
 from collections import Counter
 from rapidfuzz.fuzz import ratio
 
@@ -10,11 +9,3 @@ def list_counter(items: list) -> list:
     _dict = dict(Counter(items))
     result = sorted(_dict.items(), key=lambda x: x[1], reverse=True)
     return result
-
-def topics_ok(topic_names: list) -> bool:
-    if not topic_names:
-        return False
-    lens = [len(n) for n in topic_names]
-    avg_len = mean(lens)
-    return avg_len <= 20
-
