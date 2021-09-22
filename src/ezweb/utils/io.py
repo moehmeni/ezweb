1cfa96af4ed01c799f666f7e25d4d33897efd8c2
Mohammad Momeni <59508036+mdmomeni@users.noreply.github.com>
2021-09-22 14:25:16 +0330

    Change create_file file name to random temporarily

--- src/ezweb/utils/io.py
+++ src/ezweb/utils/io.py
@@ -1,11 +1,15 @@
+
+import random
+import string
+
 def ok_file_name(string : str):
     # https://stackoverflow.com/a/295152/12696223
     return "".join(l for l in string if l.isalnum())
 
 def create_file(path : str , content : str):
     dot_splited = path.split(".")
-    file_name = dot_splited[-2]
     file_extension = dot_splited[-1]
+    file_name = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
     path = ok_file_name(file_name) + "." + file_extension
     with open(path, mode="w", encoding="utf-8") as f:
         f.write(content)
