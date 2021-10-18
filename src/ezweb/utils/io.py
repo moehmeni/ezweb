4372d935b47524699b7e023caed77f504e9a9901
Mohammad Momeni <59508036+mdmomeni@users.noreply.github.com>
2021-10-18 18:50:04 +0330

    Some IO changes

--- src/ezweb/utils/io.py
+++ src/ezweb/utils/io.py
@@ -8,8 +8,8 @@ def ok_file_name(string : str):
 
 def create_file(path : str , content : str):
     dot_splited = path.split(".")
-    file_extension = dot_splited[-1]
-    file_name = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
+    file_name , file_extension = dot_splited[-2] , dot_splited[-1]
+    # file_name = ''.join(random.choices(string.ascii_letters + string.digits, k=5))
     path = ok_file_name(file_name) + "." + file_extension
     with open(path, mode="w", encoding="utf-8") as f:
         f.write(content)
